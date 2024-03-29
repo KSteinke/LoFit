34d92997f85d2b6f6db5f74d802e301330dcf0a5
KSteinke <kacpersteinkesb@gmail.com>
2024-03-29 17:28:57 +0100

    Test Build

--- LoFit_Server/obj/Debug/net8.0/LoFit_Server.AssemblyInfo.cs
+++ LoFit_Server/obj/Debug/net8.0/LoFit_Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LoFit_Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd3e0b8a303ac89af38220a28ef76ad6639edd0c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8dd38caefa43154c3e76dc5e3011136e33899469")]
 [assembly: System.Reflection.AssemblyProductAttribute("LoFit_Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LoFit_Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

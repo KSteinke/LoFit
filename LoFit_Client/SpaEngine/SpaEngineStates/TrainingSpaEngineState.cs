07e8d2dfbc44dec6c362a2411ad62e69add6a575
KSteinke <kacpersteinkesb@gmail.com>
2024-05-26 13:57:17 +0200

    Prev exercise button functionality added

--- LoFit_Client/SpaEngine/SpaEngineStates/TrainingSpaEngineState.cs
+++ LoFit_Client/SpaEngine/SpaEngineStates/TrainingSpaEngineState.cs
@@ -14,6 +14,7 @@ public class TrainingSpaEngineState:SpaEngineState
     {
         this.spaEngineService.NewTrainingSetDetailsDto = newTrainingSetDetailsDto;
         this.spaEngineService.TransitionTo(new SummarySpaEngineState());
+        //TODO - Delete debuging part
         Console.WriteLine("Old:");
         foreach(var x in this.spaEngineService.PrevTrainingSetDetailsDto.ExerciseDetailsDtos)
         {
